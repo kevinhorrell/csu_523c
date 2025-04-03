1c412ead7bdd80ea37f36207f85c9e7332ece9f5
Kevin Horrell <kevin.horrell@colostate.edu>
2025-04-02 21:25:03 -0600

    Edits to Lab 1, initial upload Lab 2

---
+++ docs/lab-02_files/leaflet_intro.R
@@ -0,0 +1,11 @@
+##Kevin Horrell
+##leaflet package intro
+
+install.packages('leaflet')
+install.packages('ropensci/USAboundaries')
+install.packages('ropensci/USAboundariesData')
+library(leaflet)
+library(sf)
+library(tidyverse)
+library(USAboundaries)
+library(USAboundariesData)
