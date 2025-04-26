0bb334d31544375ca8a898061c7895a5eaa730db
Kevin Horrell <kevin.horrell@colostate.edu>
2025-04-26 11:38:18 -0600

    Lab5 Initial commit

--- updateR_package_install.R
+++ updateR_package_install.R
@@ -4,3 +4,23 @@ install.packages('remotes')
 library(remotes)
 
 remotes::install_github("mikejohnson51/AOI")
+
+#data Retrieval from USGS
+install.packages('dataRetrieval')
+library(dataRetrieval)
+
+#machine learning and data manipulation
+library(tidymodels)
+
+install.packages('powerjoin')
+install.packages('glue')
+install.packages('vip')
+install.packages('baguette')
+
+
+##Data Viz
+install.packages('gghighlight')
+library(gghighlight)
+library(ggrepel)
+install.packages('ggthemes')
+library(ggthemes)
